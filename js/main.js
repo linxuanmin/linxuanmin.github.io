0985526313900ba337ca9aa8059c4d5da9b45f80
linxuanmin <3166863487@qq.com>
2023-04-19 16:25:06 +0800

    commit myPage1.0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function btnClick(){
+	window.location.href="/myPage/html/D3_universeIntroduce.html";
+	
+}
+
+function mouseOver(x) {
+	document.getElementById("myblog").innerHTML="This is my CSDN blog!";
+ }
+  
+ function mouseLeave(x) {
+	 document.getElementById("myblog").innerHTML="";
+ }
